41521263e2728dbcb27cf1186156e5f1f522f690
Sienna <remicknsisi@gmail.com>
2022-10-10 09:41:46 -0400

    commit tree d1494b2df10d5db3edeb499282b4532ed4eb4650

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const userInfo = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(userInfo),
+  };
+  
+  function submitData(){
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(object => document.querySelector('body').append(object.id))
+    .catch(function(error){
+        alert("Error detected!")
+        document.querySelector('body').append(error)
+    });
+  }
